b9d38c5bfa6afa876d30fda432ff4e7bb3e752a3
Bin Hu <elithrade@gmail.com>
2018-03-01 22:09:48 +0800

    Initial commit

---
+++ Drag and Drop/Assets/DragHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public sealed class DragHandler :
+    MonoBehaviour,
+    IBeginDragHandler,
+    IDragHandler,
+    IEndDragHandler
+{
+    public static GameObject itemBeingDragged;
+
+    private Vector3 _startPosition;
+    private Transform _startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Sets item being dragged to the current games object
+        itemBeingDragged = gameObject;
+        // Remember the start position
+        _startPosition = transform.position;
+        // Remember the start parent, slot holding the item
+        _startParent = transform.parent;
+        // Allows pass event through the item being dragged
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        itemBeingDragged = null;
+
+        if (transform.parent != _startParent)
+        {
+            return;
+        }
+
+        transform.position = _startPosition;
+    }
+}
