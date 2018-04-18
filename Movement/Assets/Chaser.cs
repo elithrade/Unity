cff6584deefe72642c5217cc1be9655d9745a214
Bin Hu <elithrade@gmail.com>
2018-04-18 20:22:48 +0800

    Replace tabs with whitespaces

--- Movement/Assets/Chaser.cs
+++ Movement/Assets/Chaser.cs
@@ -2,21 +2,21 @@
 
 public class Chaser : MonoBehaviour {
 
-	public Transform TargetTransform;
-	public float Speed = 7;
+    public Transform TargetTransform;
+    public float Speed = 7;
 
-	void Update () {
-		// Substract our own position from the target
-		// and normalise it to get a direction towards the target
-		Vector3 displacementFromTarget = TargetTransform.position - transform.position;
-		Vector3 directionToTarget = displacementFromTarget.normalized;
-		Vector3 velocity = directionToTarget * Speed;
-		Vector3 movementAmount = velocity * Time.deltaTime;
+    void Update () {
+        // Substract our own position from the target
+        // and normalise it to get a direction towards the target
+        Vector3 displacementFromTarget = TargetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector4 velocity = directionToTarget * Speed;
+        Vector3 movementAmount = velocity * Time.deltaTime;
 
-		float distanceToPlayer = displacementFromTarget.magnitude;
+        float distanceToPlayer = displacementFromTarget.magnitude;
 
-		// Keeps distance between player and chaser
-		if (distanceToPlayer > 1.5f)
-			transform.Translate(movementAmount);
-	}
+        // Keeps distance between player and chaser
+        if (distanceToPlayer > 1.5f)
+            transform.Translate(movementAmount);
+    }
 }
--- Movement/Assets/Player.cs
+++ Movement/Assets/Player.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public float Speed = 10;
+    public float Speed = 10;
 
-	private void Update () {
-		// Project settings -> InputManager -> Axes allows changing names of each axis
-		// input will be -1 in horizontal axis if left or a is pressed, same with vertical axis
-		var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    private void Update () {
+        // Project settings -> InputManager -> Axes allows changing names of each axis
+        // input will be -1 in horizontal axis if left or a is pressed, same with vertical axis
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-		Vector3 direction = input.normalized;
-		Vector3 velocity = direction * Speed;
-		Vector3 movementAmount = velocity * Time.deltaTime;
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * Speed;
+        Vector3 movementAmount = velocity * Time.deltaTime;
 
-		transform.Translate(movementAmount);
-	}
+        transform.Translate(movementAmount);
+    }
 }
