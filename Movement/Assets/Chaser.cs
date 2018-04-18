ec2255c3fd8cd8e876c804af1b88515889761464
Bin Hu <elithrade@gmail.com>
2018-04-18 08:03:05 +0800

    Initial commit

---
+++ Movement/Assets/Chaser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+	public Transform TargetTransform;
+	public const float Speed = 7;
+
+	void Update () {
+		// Substract our own position from the target
+		// and normalise it to get a direction towards the target
+		Vector3 displacementFromTarget = TargetTransform.position - transform.position;
+		Vector3 directionToTarget = displacementFromTarget.normalized;
+		Vector3 velocity = directionToTarget * Speed;
+		Vector3 movementAmount = velocity * Time.deltaTime;
+
+		float distanceToPlayer = displacementFromTarget.magnitude;
+
+		if (distanceToPlayer > 1.5f)
+			transform.Translate(movementAmount);
+	}
+}
---
+++ Movement/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public const float Speed = 10;
+	private void Update () {
+		// Project settings -> InputManager -> Axes allows changing names of each axis
+		// input will be -1 in horizontal axis if left or a is pressed, same with vertical axis
+		var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		Vector3 direction = input.normalized;
+		Vector3 velocity = direction * Speed;
+		Vector3 movementAmount = velocity * Time.deltaTime;
+
+		transform.Translate(movementAmount);
+	}
+}
