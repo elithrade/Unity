4e67cc8b886e4747251c8fa89d42148fb4584f44
Bin Hu <elithrade@gmail.com>
2018-04-19 23:47:27 +0800

    Add BlockSpawner script

---
+++ FallingBlocks/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject FallingBlockPrefab;
+    private Vector2 _screenHalfSizeInWorldUnits;
+    private float _secondsBetweenSpawnTime = 1;
+    private float _nextSpawnTime;
+
+    private void Start ()
+    {
+        float screenHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+        float screenHalfHeight = Camera.main.orthographicSize;
+
+        _screenHalfSizeInWorldUnits = new Vector2(screenHalfWidth, screenHalfHeight);
+    }
+
+    private void Update ()
+    {
+        float now = Time.time;
+
+        if (now > _nextSpawnTime)
+        {
+            _nextSpawnTime = now + _secondsBetweenSpawnTime;
+
+            float randomX = Random.Range(-_screenHalfSizeInWorldUnits.x, _screenHalfSizeInWorldUnits.x);
+            float y = _screenHalfSizeInWorldUnits.y;
+            Vector2 spawnPosition = new Vector2(randomX, y);
+
+            Instantiate(FallingBlockPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ FallingBlocks/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public float Speed = 7;
+    
+    void Update ()
+    {
+        transform.Translate(Vector2.down * Speed * Time.deltaTime);
+    }
+}
