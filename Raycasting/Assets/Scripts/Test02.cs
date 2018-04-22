1f5a4f22a912e815691fa28c1dd1058418230aa7
Bin Hu <elithrade@gmail.com>
2018-04-22 14:53:36 +0800

    Raycast hit terrain test

---
+++ Raycasting/Assets/Scripts/Test02.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    public Transform ObjectToPlace;
+    public Camera GameCamera;
+    
+    void Update ()
+    {
+        Ray ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            ObjectToPlace.position = hitInfo.point;
+            // Create rotation from the up to direction of the surface normal
+            ObjectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+}
