1635ed2f372d50b4067c30b20df9d19b06644bc9
Bin Hu <elithrade@gmail.com>
2018-04-23 23:49:03 +0800

    Initial commit

---
+++ Stealth/Assets/Scripts/Guard.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public Transform Path;
+    public float Speed = 7;
+    public float WaitTime = .2f;
+    // 90 degrees per seconds
+    public float TurnSpeed = 90;
+
+    private void Start()
+    {
+        Vector3[] waypoints = new Vector3[Path.childCount];
+        for (int i = 0; i < Path.childCount; i++)
+        {
+            var childPosition = Path.GetChild(i).position;
+            // Change waypoint height to be the same as transform
+            waypoints[i] = new Vector3(childPosition.x, transform.position.y, childPosition.z);
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    IEnumerator FollowPath(Vector3[] path)
+    {
+        transform.position = path[0];
+
+        int nextWaypointIndex = 1;
+        Vector3 targetWaypoint = path[nextWaypointIndex];
+        transform.LookAt(targetWaypoint);
+
+        for(;;)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, Speed * Time.deltaTime);
+            if (transform.position == targetWaypoint)
+            {
+                // Ensure we go back to the beginning once we reached the last waypoint
+                nextWaypointIndex = (nextWaypointIndex + 1) % path.Length;
+                targetWaypoint = path[nextWaypointIndex];
+                // Wait for half a second once we reached the target
+                yield return new WaitForSeconds(WaitTime);
+                // Wait for rotation to finish
+                yield return StartCoroutine(Turn(targetWaypoint));
+            }
+            // Wait for one frame
+            yield return null;
+        }
+    }
+
+    IEnumerator Turn(Vector3 lookAt)
+    {
+        Vector3 directionToTarget = (lookAt - transform.position).normalized;
+        float angle = Mathf.Atan2(directionToTarget.z, directionToTarget.x) * Mathf.Rad2Deg;
+        // Note this doesn't work with angle greater than 180
+        float targetAngle = 90 - angle;
+        // y is the axis we rotate around
+        while (Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)
+        {
+            float turnAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, TurnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * turnAngle;
+            yield return null;
+        }
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = Path.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in Path)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            if (previousPosition == waypoint.position)
+                continue;
+
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+}
