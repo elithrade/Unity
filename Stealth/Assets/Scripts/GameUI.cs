689fe86cdffcec401d22937f39d46f338bb8c845
Bin Hu <elithrade@gmail.com>
2018-04-27 00:04:36 +0800

    Unsubscribe event once game is over; switch off auto generate lighting to fix black scene after reload

--- Stealth/Assets/Scripts/GameUI.cs
+++ Stealth/Assets/Scripts/GameUI.cs
@@ -24,6 +24,7 @@ public class GameUI : MonoBehaviour
     {
         _gameIsOver = true;
         gameOverUI.SetActive(true);
+        Guard.OnPlayerSpotted -= OnPlayerSpotted;
     }
 
     void Update()
