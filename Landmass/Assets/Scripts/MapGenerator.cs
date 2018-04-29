74771124db89710dac221eb3033836599191c432
Bin Hu <elithrade@gmail.com>
2018-04-29 23:47:59 +0800

    Implement OnValidate

--- Landmass/Assets/Scripts/MapGenerator.cs
+++ Landmass/Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,7 @@ public class MapGenerator : MonoBehaviour
     public int Height;
     public float Scale;
     public int Octave;
+    [Range(0, 1)]
     public float Persistence;
     public float Lacunarity;
     public int Seed;
@@ -24,5 +25,17 @@ public class MapGenerator : MonoBehaviour
             return;
 
         display.Draw(noiseMap);
+    }
+
+    public void OnValidate()
+    {
+        if (Width < 1)
+            Width = 1;
+        if (Height < 1)
+            Height = 1;
+        if (Octave < 1)
+            Octave = 1;
+        if (Lacunarity < 1)
+            Lacunarity = 1;
     }
 }
